c96bf51123efc45c3f62e5eea421a485c1c5f6d4
prasannashrestha1 <97834918+prasannashrestha1@users.noreply.github.com>
2025-01-05 19:55:25 +0545

    connect server with database.

--- api/index.js
+++ api/index.js
@@ -1,7 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Connected to the server and running on 3000");
-});
+const connect = async () => {
+  await mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+      console.log("connected to db successfully");
+    })
+    .catch((err) => {
+      console.error("Database connection error:", err);
+    });
+
+  app.listen(3000, () => {
+    console.log("Connected to the server and running on 3000");
+  });
+};
+
+connect();
